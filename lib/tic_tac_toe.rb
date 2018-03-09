db965b39cbd0a1e1ba9c97c9201ab42aa1e784e9
michael hohl <mhohl@rocketmail.com>
2018-03-09 20:50:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,10 +30,9 @@ class TicTacToe
     board[location] != " " && board[location] != ""
   end
   def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
+   index.between?(0,8) && !position_taken?(board, index)
+  end
+  def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
